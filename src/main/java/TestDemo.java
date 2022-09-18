007c23d17c0292771e354d75b4cd7205afb8e56d
johnnylee-55 <leejohnny5533@gmail.com>
2022-09-17 18:49:21 -0700

    first commit

---
+++ src/main/java/TestDemo.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+
+public class TestDemo {
+
+	public int addPositive(int a, int b) {
+		
+		if (a > 0 && b > 0) {
+			return a + b;
+		} else {
+			throw new IllegalArgumentException("Both parameters must be positve!");
+		}
+	}
+	
+	public int randomNumberSquared() {
+		int x = getRandomInt();
+		return x * x; 
+	}
+	
+	int getRandomInt() {
+		Random random = new Random();
+		return random.nextInt(10) + 1;
+	}
+}
